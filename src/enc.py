ce9542eb1fa542b2bbff43749d67c7fb549ab0fb
zahar <ipad32nikita@gmail.com>
2024-05-12 21:10:39 +0300

    add aes

--- src/enc.py
+++ src/enc.py
@@ -1,9 +1,34 @@
-from typing import Dict
+from typing import Dict, Union
+from .aes import encrypt, decrypt
+import hashlib, hmac
+import random
+
+hash_function = hash_function = hashlib.sha256
 
 
-def encode(msg: str) -> Dict[str, str]:
-    pass
+def encode_msg(msg: str, key: int) -> Dict[str, Union[str, bytes]]:
+    return {
+        "hash": hmac.new(str(key).encode(), msg.encode(), hash_function).hexdigest(),
+        "message": encrypt(str(key).encode(), msg)
+    }
 
 
-def decode(enc: Dict[str, str]) -> str:
-    pass
+def decode_msg(enc: Dict[str, Union[str, bytes]], key: int) -> str:
+    return decrypt(str(key).encode(), enc["message"]).decode()
+
+
+MY_KEY=None
+G=9
+P=2*255-19
+
+
+def encode_key() -> Dict[str, int]:
+    MY_KEY = random.randint(2, P - 1)
+    encoded = pow(9, MY_KEY, P)
+    return {
+        "key": encoded,
+    }
+
+
+def decode_key(enc: Dict[str, int]) -> int:
+    return pow(enc["key"], MY_KEY, P)
